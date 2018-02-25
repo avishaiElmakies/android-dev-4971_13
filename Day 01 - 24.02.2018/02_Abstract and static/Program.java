3dc03c795ed0086a36a247315255a3219c8c805f
Anna Karp <32166631+AnnaKarpf@users.noreply.github.com>
2018-02-25 15:27:08 +0200

    Update Program.java

--- Day 01 - 24.02.2018/02_Abstract and static/Program.java
+++ Day 01 - 24.02.2018/02_Abstract and static/Program.java
@@ -1,14 +1,14 @@
 /*
-						|	static	function	|	non-static
-__________________________________________________________________
-static property			|	yes					|	yes
-__________________________________________________________________
-static function			|	yes					|	yes
-__________________________________________________________________
-non-static property		|	no					|	yes
-__________________________________________________________________
-non-static function 	|	no					|	yes
-__________________________________________________________________
+				|	static	function	|	non-static
+_________________________________________________________________________________________
+static property			|	yes			|	yes
+_________________________________________________________________________________________
+static function			|	yes			|	yes
+_________________________________________________________________________________________
+non-static property		|	no			|	yes
+_________________________________________________________________________________________
+non-static function 		|	no			|	yes
+_________________________________________________________________________________________
  */
 		
 
@@ -40,19 +40,19 @@ public class Program {
 	
 	public static void main(String[] args) {
 	
-		address a;
-		person[] personArr=new person[5];
+		Address a;
+		Person[] personArr=new Person[5];
 		
 		String nameSrc,schoolSrc;
 		
 		
-		System.out.println(person.getCounter());
-		a=new address(13,"telaviv","israel");		
+		System.out.println(Person.getCounter());
+		a=new Address(13,"telaviv","israel");		
 		nameSrc="Aviv";
 		schoolSrc="john bryce";
 	
-		personArr[0]=new student(18,nameSrc,a,80,schoolSrc);	
-		personArr[1]=new student(22,"Elio",a,90,schoolSrc);
+		personArr[0]=new Student(18,nameSrc,a,80,schoolSrc);	
+		personArr[1]=new Student(22,"Elio",a,90,schoolSrc);
 		personArr[2]=new Manager(22,"Elio",a,"Google");
 		//personArr[2]=new person(22,"Elio",a);  --> compilation error (can not create an object from abstract class)
 		personArr[4]=new Manager(22,"Elio",a,"Microsoft");
@@ -60,23 +60,23 @@ public class Program {
 		a.setCity("Haifa");
 		nameSrc="Dor";
 		
-for(person p :personArr){
+	for(Person p :personArr){
 	//The method getGrade() is undefined for the type person
 	//System.out.println(p.getGrade()); -->compilation ERROR
 	
-	
-	try{
-		//checking if p1 in the stack points to a student object in heap
-		if(p instanceof student){
-			System.out.println(((student)p).getGrade());
-		}
 
-		System.out.println(p.sayHello()); 
-		
-	}
-	catch(Exception ex){
-		System.out.println("element is null"); 
-	}
+		try{
+			//checking if p1 in the stack points to a student object in heap
+			if(p instanceof Student){
+				System.out.println(((Student)p).getGrade());
+			}
+
+			System.out.println(p.sayHello()); 
+
+		}
+		catch(Exception ex){
+			System.out.println("element is null"); 
+		}
 	System.out.println("-------------------------------------"); 
 }
 		
