a7129f2077cd748ac9890fed3bdbc3b56b4e77e7
Anna Karp <32166631+AnnaKarpf@users.noreply.github.com>
2018-02-26 15:40:34 +0200

    android

--- Day 02 - 26.02.2018/05 - Set Dimensions Programmatically/android/app/src/main/java/com/karp/anna/myapplication/MainActivity.java
+++ Day 02 - 26.02.2018/05 - Set Dimensions Programmatically/android/app/src/main/java/com/karp/anna/myapplication/MainActivity.java
@@ -8,8 +8,7 @@ import android.util.TypedValue;
 import android.view.View;
 import android.widget.TextView;
 
-// Programmatically, there is only px units.
-// Thus we need to convert any other units to pixels.
+
 public class MainActivity extends AppCompatActivity {
 
     private TextView textViewDynamic;
@@ -27,15 +26,15 @@ public class MainActivity extends AppCompatActivity {
 
 
         // Get TextView's Width :
-        float dp = textViewDynamic.getWidth();
+        float px = textViewDynamic.getWidth();
 
-        // Convert 100dp to pixels:
+        // Convert 10dp to pixels:
         float pixels = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 10, displayMetrics);
 
         // Set TextView's Width in Pixels:
-        textViewDynamic.setWidth(Math.round(pixels+dp));
+        textViewDynamic.setWidth(Math.round(pixels+px));
 
         // Set TextView's Text:
-        textViewDynamic.setText(Math.round(pixels+dp)+"dp");
+        textViewDynamic.setText(Math.round(pixels+px)+"px");
     }
 }
